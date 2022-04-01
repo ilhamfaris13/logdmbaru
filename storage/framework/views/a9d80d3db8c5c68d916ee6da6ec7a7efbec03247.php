328ecb22fcccb717095dee0a6fe0986c5ad3d0bd
Muhammad Ilham Faris <53896024+ilhamfaris13@users.noreply.github.com>
2022-04-01 17:57:24 +0700

    update_010422

--- storage/framework/views/a9d80d3db8c5c68d916ee6da6ec7a7efbec03247.php
+++ storage/framework/views/a9d80d3db8c5c68d916ee6da6ec7a7efbec03247.php
@@ -1,6 +1,6 @@
-@extends('admin.master')
 
-@section('content')
+
+<?php $__env->startSection('content'); ?>
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-6">
@@ -17,6 +17,7 @@
         <div class="col-md-6">
         
         </div>
+        </div>
     </div>
-</div>
-    @endsection
+    <?php $__env->stopSection(); ?>
+<?php echo $__env->make('admin.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\logdmbaru\resources\views//rekmedis/index.blade.php ENDPATH**/ ?>
